cde65369f1e68a6bbc2c70365cb03aad995a3c55
Hieu Minh <info.hieuit@gmail.com>
2017-06-28 15:03:53 +0700

    init models

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :comments
+  has_many :active_relationships, class_name: "Relationship",
+    foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: "Relationship",
+    foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  validates :full_name, presence: true,
+    length: {maximum: Settings.user.full_name.max_length}
+
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable
+end
