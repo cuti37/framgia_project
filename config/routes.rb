9660bd70fcbf3edcab7d538448d1ea516df5bf8c
Hieu Minh <info.hieuit@gmail.com>
2017-07-03 06:48:28 +0700

    push sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get "users/new"
+
   get "static_pages/home"
 
   get "static_pages/help"
 
   root to: "static_pages#home"
-  devise_for :users
+
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
 end
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,18 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe StaticPagesController, type: :controller do
 
   describe "GET #home" do
     it "returns http success" do
       get :home
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status :success
     end
   end
 
   describe "GET #help" do
     it "returns http success" do
       get :help
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status :success
     end
   end
 
